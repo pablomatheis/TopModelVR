4ac9cc0b43bd013bd8f1ee2f9b586c69f2013250
Pablo Matheis <pablomatheis@Pablos-MacBook-Air.local>
2024-05-29 14:13:05 +0200

    remove scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {VideoGridItem} from "./components/VideoGridItem.tsx";
 
 export default function App() {
     const [selectedCategory, setSelectedCategory] = useState(categories[0])
-    return <div className="max-h-screen flex flex-col">
+    return <div className="max-h-screen flex flex-col scrollbar-hide">
         <PageHeader />
         <div className={'grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto'}>
             <div className={'overflow-x-hidden px-5 rounded-lg'}>
