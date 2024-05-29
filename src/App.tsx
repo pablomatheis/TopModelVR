4e83e8e33991ef7b1b0f61282120327356f6d0e4
Pablo Matheis <pablomatheis@Pablos-MacBook-Air.local>
2024-05-29 14:45:24 +0200

    centered categories

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
         <PageHeader />
         <div className={'grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto scrollbar-hide'}>
             <div className={'overflow-x-hidden px-5 rounded-lg scrollbar-hide'}>
-                <div className={'flex sticky top-0 z-10 py-3 bg-black bg-opacity-85 scrollbar-hide'}>
+                <div className={'flex sticky top-0 z-10 py-3 bg-black bg-opacity-85 scrollbar-hide justify-center'}>
                     <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory}/>
                 </div>
                 <div className={'grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))] scrollbar-hide'}>
