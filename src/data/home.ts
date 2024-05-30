ffaed2a91a193e92aab54f50ee348be6f0b9db46
Pablo Matheis <pablomatheis@Pablos-MacBook-Air.local>
2024-05-30 17:45:15 +0200

    cow

--- src/data/home.ts
+++ src/data/home.ts
@@ -16,7 +16,7 @@ export const categories = [
 export const videos = [
   {
     id: "1",
-    title: "Harley Quinn Suicide Squad Airport Scene",
+    title: "Cow",
     channel: {
       name: "TopModelVR",
       id: "TopModelVR",
@@ -28,54 +28,6 @@ export const videos = [
     duration: 938,
     thumbnailUrl: "https://res.cloudinary.com/dti1ucefi/image/upload/v1716997102/harleyquinn_thumbnail.png",
     videoUrl:
-      "https://res.cloudinary.com/dti1ucefi/video/upload/v1716995169/harleyquinn_scene_test.mp4",
-  },
-  {
-    id: "2",
-    title: "Harley Quinn Suicide Squad Airport Scene",
-    channel: {
-      name: "TopModelVR",
-      id: "TopModelVR",
-      profileUrl:
-        logo,
-    },
-    views: 1222536,
-    postedAt: new Date("2023-08-29"),
-    duration: 938,
-    thumbnailUrl: "https://res.cloudinary.com/dti1ucefi/image/upload/v1716997102/harleyquinn_thumbnail.png",
-    videoUrl:
-      "https://res.cloudinary.com/dti1ucefi/video/upload/v1716995169/harleyquinn_scene_test.mp4",
-  },
-  {
-    id: "3",
-    title: "Harley Quinn Suicide Squad Airport Scene",
-    channel: {
-      name: "TopModelVR",
-      id: "TopModelVR",
-      profileUrl:
-        logo,
-    },
-    views: 1222536,
-    postedAt: new Date("2023-08-29"),
-    duration: 938,
-    thumbnailUrl: "https://res.cloudinary.com/dti1ucefi/image/upload/v1716997102/harleyquinn_thumbnail.png",
-    videoUrl:
-      "https://res.cloudinary.com/dti1ucefi/video/upload/v1716995169/harleyquinn_scene_test.mp4",
-  },
-  {
-    id: "4",
-    title: "Harley Quinn Suicide Squad Airport Scene",
-    channel: {
-      name: "TopModelVR",
-      id: "TopModelVR",
-      profileUrl:
-        logo,
-    },
-    views: 1222536,
-    postedAt: new Date("2023-08-29"),
-    duration: 938,
-    thumbnailUrl: "https://res.cloudinary.com/dti1ucefi/image/upload/v1716997102/harleyquinn_thumbnail.png",
-    videoUrl:
-      "https://res.cloudinary.com/dti1ucefi/video/upload/v1716995169/harleyquinn_scene_test.mp4",
-  },
+      "https://res.cloudinary.com/dti1ucefi/video/upload/v1717083869/15921892-hd_1280_720_50fps.mp4",
+  }
 ]
